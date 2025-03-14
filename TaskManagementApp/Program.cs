d27c892a0b764d244f34e3e301a59864b70fd782
Ranjeet Singh <Ranjeets999@outlook.com>
2025-03-14 13:08:07 +0530

    change <IdentityUser> to <User> in Program.cs and _LoginPartial.cshtml to remove userManage error

--- TaskManagementApp/Program.cs
+++ TaskManagementApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TaskManagementApp.Common;
 using TaskManagementApp.Data;
+using TaskManagementApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,8 +15,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
